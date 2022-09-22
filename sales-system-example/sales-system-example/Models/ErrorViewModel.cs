b37a1e3ac50a13a785e9786cf5e4f5ac4d71474b
Leandro Henrique <leandro.pinheiro@santacasa.pa.gov.br>
2022-09-21 22:34:19 -0300

    Commit inicial

---
+++ sales-system-example/sales-system-example/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sales_system_example.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

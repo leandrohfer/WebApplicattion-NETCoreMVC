0164311a009eff80632d56ca1ac8b6e37f5f6e22
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2022-09-27 23:04:52 -0300

    Adição da função de deletar Vendedor (Seller)
    Adição de dois métodos (FindById e Remove) na classe SellerService; criação de dois métodos Delete IActionResult na controladora de vendedores (sellersController) tanto para o método GET quanto para o método POST; e criação e ajuste da View de confirmação do Delete.

--- sales-system-example/sales-system-example/Controllers/SellersController.cs
+++ sales-system-example/sales-system-example/Controllers/SellersController.cs
@@ -38,5 +38,32 @@ namespace sales_system_example.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            // Como o Id é opcional, o tipo da variável é nullable, então
+            // precisamos usar o .Value na variável.
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }

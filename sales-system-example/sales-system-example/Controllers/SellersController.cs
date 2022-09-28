2792f640540854fd342bab89ca924be1e808c430
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2022-09-27 23:47:29 -0300

    Adição da função Details e configuração do eager loading
    Também foi criado uma view para details

--- sales-system-example/sales-system-example/Controllers/SellersController.cs
+++ sales-system-example/sales-system-example/Controllers/SellersController.cs
@@ -65,5 +65,22 @@ namespace sales_system_example.Controllers
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
